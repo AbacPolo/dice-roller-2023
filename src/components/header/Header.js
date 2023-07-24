3efac319afd4bb97f46b49fbb7d9dc87b2c18a84
Agutr0t <abak.93@gmail.com>
2023-07-24 10:14:19 +0200

    set-up project and dependencies

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header_Container">
+      <div className="Header_Wrapper"></div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/routes/board/Board.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Board.css";
+import Header from '../../components/header/Header'
+
+function Board() {
+  return (
+    <div className="Board_Container">
+      <div className="Board_Wrapper">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default Board;
