3df1a3abedcfd99baa1add6e1404d8aae92d511e
Agutr0t <abak.93@gmail.com>
2023-07-24 12:48:30 +0200

    feat: Header first iteration

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,32 @@
 import React from "react";
 import "./Header.css";
+import { IconButton, ThemeProvider } from "@mui/material";
+import {
+  AccountCircleOutlined,
+  Login,
+  LoginOutlined,
+} from "@mui/icons-material";
+import { theme } from "../../styles/theme";
+import logoImage from "../../images/role-playing.png";
 
 function Header() {
   return (
-    <div className="Header_Container">
-      <div className="Header_Wrapper"></div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="Header_Container" color="neutral.backgroundColor">
+        <div className="Header_Wrapper">
+          <IconButton color="primary" className="Login_Button">
+            <LoginOutlined />
+          </IconButton>
+          <div className="Title_Container">
+            <h1>DICE ROLLER</h1>
+            <img src={logoImage} className="Logo_Image" alt="Logo Dice"></img>
+          </div>
+          <IconButton color="primary" disabled>
+            <AccountCircleOutlined />
+          </IconButton>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
