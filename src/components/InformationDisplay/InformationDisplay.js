9bd955c653fb27c86f44d8d0b08b4e7bf3efadfb
Agutr0t <abak.93@gmail.com>
2023-08-01 12:28:28 +0200

    feat: howToUse menu for mobile and desktop

--- src/components/InformationDisplay/InformationDisplay.js
+++ src/components/InformationDisplay/InformationDisplay.js
@@ -6,14 +6,15 @@ function InformationDisplay() {
     <div className="InformationDisplay_Container">
       <div className="InformationDisplay_Wrapper">
         <h3>How to use DICE THROWER</h3>
-        <section>
+        <section className="InformationDisplay_Section">
             <h4>Valid Throws examples:</h4>
             <ul>
-                <li><b>1d20+5</b> (Dice + Modifier)</li>
-                <li><b>1d20+1d4</b> (Dice + Dice)</li>
-                <li><b>2d20</b> (Multiple Dice)</li>
-                <li><b>2d20adv</b> (Multiple Dice with adventage)</li>
-                <li><b>2d20dis</b> (Multiple Dice with disadventage)</li>
+                <li><b>1d20</b> (One dice)</li>
+                <li><b>1d20+1d4</b> (Multiple dice with different sizes)</li>
+                <li><b>2d20</b> (Multiple dice with the same size)</li>
+                <li><b>1d20+5</b> (One or more dice + Modifier)</li>
+                <li><b>2d20adv</b> (Multiple dice with the same size with adventage)</li>
+                <li><b>2d20dis</b> (Multiple dice with the same size with disadventage)</li>
                 <li>& combinations of all the options above</li>
             </ul>
         </section>
