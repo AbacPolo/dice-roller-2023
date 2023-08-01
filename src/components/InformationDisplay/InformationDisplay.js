4c55d2c0e3453448495d8aeaec5bfce52b9fbaa5
Agutr0t <abak.93@gmail.com>
2023-08-01 12:30:04 +0200

    feat: typo fix on informationdisplay

--- src/components/InformationDisplay/InformationDisplay.js
+++ src/components/InformationDisplay/InformationDisplay.js
@@ -10,11 +10,11 @@ function InformationDisplay() {
             <h4>Valid Throws examples:</h4>
             <ul>
                 <li><b>1d20</b> (One dice)</li>
-                <li><b>1d20+1d4</b> (Multiple dice with different sizes)</li>
-                <li><b>2d20</b> (Multiple dice with the same size)</li>
+                <li><b>1d20+1d4</b> (Multiple dice of different sizes)</li>
+                <li><b>2d20</b> (Multiple dice of the same size)</li>
                 <li><b>1d20+5</b> (One or more dice + Modifier)</li>
-                <li><b>2d20adv</b> (Multiple dice with the same size with adventage)</li>
-                <li><b>2d20dis</b> (Multiple dice with the same size with disadventage)</li>
+                <li><b>2d20adv</b> (Multiple dice of the same size with adventage)</li>
+                <li><b>2d20dis</b> (Multiple dice of the same size with disadventage)</li>
                 <li>& combinations of all the options above</li>
             </ul>
         </section>
